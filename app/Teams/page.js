edb34ff80de258ab99e2cfa40b7dd501eef32fe6
na-klaus <ansarinavish571@gmail.com>
2024-06-29 00:34:09 +0530

    Initial commit

--- app/Teams/page.js
+++ app/Teams/page.js
@@ -1,9 +1,9 @@
-import Detailed_Teams from '../components/pages/teams/Detailed_teams.js'
+import Detailed_teams from '../components/pages/teams/Detailed_teams.js'
 
 const Process_Page = () => {
     return (
         <>
-            <Detailed_Teams />
+            <Detailed_teams />
         </>
     )
 }
