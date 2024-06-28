07831bcda5192148ec1d9dc8aec09a6d1b35eca9
NAVISH ANSARI <ansarinavish571@gmail.com>
2024-06-29 00:57:19 +0530

    Add files via upload

---
+++ app/teams/page.js
@@ -0,0 +1,11 @@
+import Detailed_teams from '../components/pages/teams/Detailed_teams.js'
+
+const Process_Page = () => {
+    return (
+        <>
+            <Detailed_teams />
+        </>
+    )
+}
+
+export default Process_Page
